7ad50e07159f621b78a72e99721c46b29d9d3f7d
Kamal Selvaraj <kamalselvaraj@Kamals-MacBook-Pro.local>
2022-05-03 18:38:51 +0530

    Fixes footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import '../App.css';
 const Footer = () => {
     return (
         <footer className="footer">
-        <a href="#" id="home">Home</a>
-        <a href="#" id="aboutus">About Us</a>
+            <p>Copyright &copy; 2022</p>
+            <a href="/about" id="aboutus">About Us</a>
         </footer>
     )
 }
