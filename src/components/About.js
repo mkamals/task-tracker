6ffa13930dd6203ed20e28b849bc735fb36b47b5
mkamals1989 <mkamals1989@gmail.com>
2022-05-05 00:41:37 +0530

    Adding About component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+
+
+
+const About = () => {
+    return (
+        <div>
+            <h3>About Us</h3>
+            <p>Hi, I'm Kamal Selvaraj. I'm a senior mobile application developer with experience in native android application and hybrid framework like React Native.<br>
+            Currently, I'm learning ReactJS to enhance my skill sets and explore the areas around web development.</br></p>
+            <button>Go Back</button>
+        </div>
+    )
+}
+
+export default About
